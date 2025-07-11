351678c2e3fb2225fee7c86151e7aa7832ea1ff1
alex <alexandre.solovyov@mail.ru>
2025-07-11 20:54:59 +0300

    correction of title/author

--- tags.py
+++ tags.py
@@ -2,9 +2,19 @@
 from mutagen.easyid3 import EasyID3
 import glob
 import sys
+import xml.etree.ElementTree as et
 
 
-album = f"{sys.argv[1]} -- {sys.argv[2]}"
+tree = et.parse("content.opf")
+root = tree.getroot()
+meta = root.find("{http://www.idpf.org/2007/opf}metadata")
+title = meta.find("{http://purl.org/dc/elements/1.1/}title").text
+author = meta.find("{http://purl.org/dc/elements/1.1/}creator").text.split()[-1]
+
+print(f"Found title={title} and author={author}")
+
+
+album = f"{author} -- {title}"
 artist = u"AI"
 folder = "res"
 files = sorted(glob.glob(f"{folder}/*.mp3"))
